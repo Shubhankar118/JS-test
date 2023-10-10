a9e808fc353345a972d7d99351a1ae661ea9ff54
Shubhankar118 <69741012+Shubhankar118@users.noreply.github.com>
2023-10-10 19:30:05 +0000

    conversion of data type

---
+++ .devcontainer/basic/var.js
@@ -0,0 +1,10 @@
+const accountID = 144553
+let accountEmail="shub@gmail.com"
+var accountPass= "12345"
+accountCity = "Asansol"
+
+// accountID = 223145
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPass]);
